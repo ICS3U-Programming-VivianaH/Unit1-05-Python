1e8ab87db7133d779fba700a41f92b71452b45c2
Viviana Hurtado <viviana.hurtado-antiporta@stu.ocsb.ca>
2025-02-19 00:57:45 +0000

    Added area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,13 @@
+# Created by: Viviana Hurtado
+# Date: may 18 2025
+# This programs calculates the area amd perimeter of a rectangle with dimensions 5cm x 3cm
+def main():
+    print("The rectangle has the dimensions:")
+    print("If the length 5cm and the width is 3cm")
+    print("")
+    print("Area is {}cm^2".format(5 * 3))
+    print("Perimeter is {}cm".format(2 * (5 + 3)))
+
+
+if __name__ == "__main__":
+    main()
